9a19d726c3f8f0e683170aaa22d9297e5bf62f3e
Dylan Elliott <dylan.elliott@gmail.com>
2026-01-13 01:19:46 +1100

    Initial Commit

---
+++ PlantaxApp/PlantaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantaxAppApp.swift
+//  PlantaxApp
+//
+//  Created by Dylan Elliott on 13/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlantaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
